bbfd2fef50983ff9c50ec1ef5d1a8494409dbb7d
Silvia <ydril.linwelin@gmail.com>
2013-11-01 20:02:43 +0000

    añadido ventana y botones, falta arreglar cuando clickas que haga algo

--- include/graphicEngine.h
+++ include/graphicEngine.h
@@ -1,10 +1,13 @@
 // Facade pattern to separate graphic engine from the rest of the game
 #include <irrlicht.h>
+
 using namespace irr;
 using namespace core;
 using namespace scene;
 using namespace video;
 using namespace io;
+using namespace irr;
+using namespace gui;
 
 class graphicEngine {
 public:
@@ -13,10 +16,74 @@ public:
 	graphicEngine(const graphicEngine&) {};
 	graphicEngine& operator=(const graphicEngine&) {};
 	bool updateOnce();
+	void pintarMenu();
+	void pintarCubo();
 
 private:
 	IrrlichtDevice * device;
 	ISceneManager * smgr;
 	IVideoDriver * driver;
 	ISceneNode * node;
+	IGUIEnvironment* env;
+	s32				counter;
+	IGUIListBox*	listbox;
+	int dimensionPantallaX; 
+	int dimensionPantallaY; 
+	IGUISkin* skin;
+	IGUIFont* font;
+};
+enum{
+	GUI_BOTON_JUGAR = 101,
+	GUI_BOTON_OPCIONES,
+	GUI_BOTON_CREDITOS,
+	GUI_BOTON_SALIR
+};
+struct SAppContext
+{
+	IrrlichtDevice *device;
+
+};
+
+class MyEventReceiver : public IEventReceiver
+{
+public:
+	MyEventReceiver(SAppContext & context) : Context(context) { }
+
+	virtual bool OnEvent(const SEvent& event)
+	{
+		printf("Hola");
+	/*
+		if (event.EventType == EET_GUI_EVENT)
+		{
+			s32 id = event.GUIEvent.Caller->getID();
+			IGUIEnvironment* env = Context.device->getGUIEnvironment();
+
+			switch(event.GUIEvent.EventType)
+			{
+
+			case GUI_BOTON_JUGAR:
+				
+				break;
+
+			case GUI_BOTON_OPCIONES:					
+			
+				break;
+
+			case GUI_BOTON_CREDITOS:
+				
+				break;
+			case GUI_BOTON_SALIR:
+				//Context.device->closeDevice();
+				break;
+
+			default:
+				break;
+			}
+		}
+*/
+		return false;
+	}
+
+private:
+	SAppContext & Context;
 };
--- src/graphicEngine.cpp
+++ src/graphicEngine.cpp
@@ -2,10 +2,52 @@
 #include "graphicEngine.h"
 
 graphicEngine::graphicEngine(){
+    dimensionPantallaX=800;
+    dimensionPantallaY=600;
+
 	//Create an Irrlicht Device.
-    device = createDevice(EDT_OPENGL,dimension2d<u32>(800,600));
+    device = createDevice(EDT_OPENGL,dimension2d<u32>(dimensionPantallaX,dimensionPantallaY));
+
+    env = device->getGUIEnvironment();
+    device->setWindowCaption(L"Demo de Mortal Village");
+    device->setResizable(true);
 
     //Get the Scene Manager from the device.
+    smgr = device->getSceneManager();
+
+    //Get the Video Driver from the device.
+    driver = device->getVideoDriver();
+
+    video::IVideoDriver* driver = device->getVideoDriver();
+    env = device->getGUIEnvironment();
+    pintarMenu();
+   
+    //pintar un cubo
+    /*
+   //Get the Scene Manager from the device.
+    smgr = device->getSceneManager();
+
+    //Get the Video Driver from the device.
+    driver = device->getVideoDriver();
+
+    //Add a Cube to the Scene.
+    node = smgr->addCubeSceneNode();
+
+    //Needed to make the object's texture visible without a light source.
+    node->setMaterialFlag(EMF_LIGHTING, false);
+
+    //Add texture to the cube.
+    node->setMaterialTexture(0,driver->getTexture("/usr/share/irrlicht/media/wall.jpg"));
+
+    //Set cube 100 units further in forward direction (Z axis).
+    node->setPosition(vector3df(0,0,100));
+
+    //Add FPS Camera to allow movement using Keyboard and Mouse.
+    smgr->addCameraSceneNodeFPS();
+*/}
+void graphicEngine::pintarCubo(){
+
+   //Get the Scene Manager from the device.
     smgr = device->getSceneManager();
 
     //Get the Video Driver from the device.
@@ -26,7 +68,31 @@ graphicEngine::graphicEngine(){
     //Add FPS Camera to allow movement using Keyboard and Mouse.
     smgr->addCameraSceneNodeFPS();
 }
+void graphicEngine::pintarMenu(){
+    skin = env->getSkin();
+    font = env->getFont("../media/fonthaettenschweiler.bmp");
+    if (font)
+        skin->setFont(font);
 
+    skin->setFont(env->getBuiltInFont(), EGDF_TOOLTIP);
+        
+    env->addButton(rect<s32>((dimensionPantallaX/2-100),dimensionPantallaY/2,(dimensionPantallaX/2+100),dimensionPantallaY/2 + 32), 0, GUI_BOTON_JUGAR,
+        L"Jugar", L"Empezar partida");
+    env->addButton(rect<s32>((dimensionPantallaX/2-100),dimensionPantallaY/2+40,(dimensionPantallaX/2+100),dimensionPantallaY/2 + 72), 0,GUI_BOTON_OPCIONES,
+        L"Opciones", L"Opciones de juego");       
+    env->addButton(rect<s32>((dimensionPantallaX/2-100),dimensionPantallaY/2+80,(dimensionPantallaX/2+100),dimensionPantallaY/2 + 112), 0, GUI_BOTON_CREDITOS,
+        L"Creditos", L"Información del juego"); 
+     env->addButton(rect<s32>((dimensionPantallaX/2-100),dimensionPantallaY/2+120,(dimensionPantallaX/2+100),dimensionPantallaY/2 + 152), 0, GUI_BOTON_SALIR,
+        L"Salir", L"Salir del juego"); 
+    /*SAppContext context;
+    context.device = device;
+    // Then create the event receiver, giving it that context structure.
+    MyEventReceiver receiver(context);
+    // And tell the device to use our custom event receiver.
+
+    device->setEventReceiver(&receiver);
+    */
+}
 /*
 	Actualiza una vez la pantalla. 
 	Devuelve si el device esta listo
@@ -37,8 +103,9 @@ bool graphicEngine::updateOnce(){
         driver->beginScene(true,true,SColor(0,125,125,125));
 
         //Render the scene at this instant.
-        smgr->drawAll();
-
+       // smgr->drawAll();
+        
+        env->drawAll();
         //End the scene
         driver->endScene();
 
