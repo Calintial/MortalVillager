27e2002eecf55eb0350ee69f0ef13a6e07e946a6
Adrian Escolano <bl4ckf4lk0n@gmail.com>
2014-03-22 11:30:09 +0100

    Bordes pantalla completa

--- src/DebugMenu.cpp
+++ src/DebugMenu.cpp
@@ -287,7 +287,7 @@ void DebugMenu::DrawVisions()
 			{
 				for(int y = pos.Y - v_range; y <= pos.Y + v_range; y++)
 				{
-					if(x < mapa->ViewSize.Width && y < mapa->ViewSize.Height)
+					if(x < mapa->WIDTH && y < mapa->HEIGHT)
 					{
 						ITexture* vision_texture = driver->getTexture("../media/Texturas/units/vision_distance.png");
 						DrawPosition = mapa2D::getIsoFromTile(x,y);
