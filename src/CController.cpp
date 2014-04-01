e0e0426e3c65ba39b355204ee223cf93b4e8d9ad
Víctor Guzmán <fawques@gmail.com>
2014-04-01 18:59:31 +0200

    pausa del aprendizaje

--- src/CController.cpp
+++ src/CController.cpp
@@ -7,9 +7,9 @@
 //
 //-----------------------------------------------------------------------
 CController::CController(IrrlichtDevice* dev): m_NumUnidades(CParams::iNumUnidades), 
-										                     m_pGA(NULL),
-										                     m_iTicks(0),
-										                     m_iGenerations(0)
+m_pGA(NULL),
+m_iTicks(0),
+m_iGenerations(0)
 {
 	device = dev;
 	driver = device->getVideoDriver();
@@ -22,15 +22,15 @@ CController::CController(IrrlichtDevice* dev): m_NumUnidades(CParams::iNumUnidad
 
 	//initialize the Genetic Algorithm class
 	m_pGA = new CGenAlg(m_NumUnidades,
-                      CParams::dMutationRate,
-	                    CParams::dCrossoverRate,
-	                    m_NumWeightsInNN);
+		CParams::dMutationRate,
+		CParams::dCrossoverRate,
+		m_NumWeightsInNN);
 
 	//Get the weights from the GA and insert into the sweepers brains
 	m_vecThePopulation = m_pGA->GetChromos();
 
 	for (int i=0; i<m_NumUnidades; i++)
-	
+
 		m_vecUnidades[i]->PutWeights(m_vecThePopulation[i].vecWeights);
 
 	//initialize mines in random positions within the application window
@@ -47,13 +47,123 @@ CController::CController(IrrlichtDevice* dev): m_NumUnidades(CParams::iNumUnidad
 CController::~CController()
 {
 	if(m_pGA)
-  {
-    delete		m_pGA;
-  }
+	{
+		delete		m_pGA;
+	}
 
 
 }
 
+bool CController::tickRedNeuronal(){
+	outfile.open("GeneticMovimientos.txt", ios::app);
+	if (outfile.is_open())
+	{
+		outfile<<"##########################################################CController::Update:76"<<" mUnidades "<<m_NumUnidades<<" Ticks "<<m_iTicks<<"#####################################################"<<endl;
+
+	}
+
+	outfile.close();
+
+	for (int i=0; i<m_NumUnidades; ++i)
+	{
+		//Pintar();
+			//update the NN and position
+		outfile.open("GeneticMovimientos.txt", ios::app);
+		if (outfile.is_open())
+		{
+			outfile << "La unidad ANTES : "<<i<<" tiene de Fitness :"<<m_vecUnidades[i]->Fitness()<<" y esta en la posición: ("<<m_vecUnidades[i]->Position().x <<","<<m_vecUnidades[i]->Position().y<<")"<<endl;
+
+		}
+
+		outfile.close();
+		if (!m_vecUnidades[i]->Update(Matriz))
+		{
+				//error in processing the neural net
+			cout<<"Wrong amount of NN inputs!"<<endl;
+			return false;
+		}
+
+		outfile.open("GeneticMovimientos.txt", ios::app);
+		if (outfile.is_open())
+		{
+			outfile << "La unidad : "<<i<<" tiene de Fitness :"<<m_vecUnidades[i]->Fitness()<<" y esta en la posición: ("<<m_vecUnidades[i]->Position().x <<","<<m_vecUnidades[i]->Position().y<<")"<<endl;
+
+		}
+
+		outfile.close();
+
+		if(m_vecUnidades[i]->getAtaque()==1){
+			SVector2D atacando=m_vecUnidades[i]->getAtaqueMovimiento();
+
+			if(Matriz[atacando.y][atacando.x]!=NULL && Matriz[atacando.y][atacando.x]->getTipo()==3){
+				m_vecUnidades[i]->IncrementFitness();
+				
+				outfile.open("GeneticMovimientos.txt", ios::app);
+				if (outfile.is_open())
+				{
+
+					outfile << "Y esta atacando a: ("<<atacando.x<<","<<atacando.y<<")"<<endl;
+				}
+
+				outfile.close();
+				
+			}
+
+			
+		}
+		else{
+
+			if(m_vecUnidades[i]->getMover()==1){
+				video::IVideoDriver* driver = device->getVideoDriver();
+				Matriz[m_vecUnidades[i]->Position().y][m_vecUnidades[i]->Position().x]= new Suelo(0,m_vecUnidades[i]->Position().x,m_vecUnidades[i]->Position().y);
+				((Suelo*) Matriz[m_vecUnidades[i]->Position().y][m_vecUnidades[i]->Position().x])->setIsometric(false);
+				Matriz[m_vecUnidades[i]->Position().y][m_vecUnidades[i]->Position().x]->aplicarTextura(driver);
+				m_vecUnidades[i]->setPosition(m_vecUnidades[i]->getMovimiento());
+				Matriz[m_vecUnidades[i]->Position().y][m_vecUnidades[i]->Position().x]=m_vecUnidades[i];
+			}
+		}
+
+
+
+
+			//update the chromos fitness score
+		m_vecThePopulation[i].dFitness = m_vecUnidades[i]->Fitness();
+
+	}
+}
+
+bool CController::redNeuronal(){
+	bool ticks = m_iTicks++ < CParams::iNumTicks;
+	if (ticks)
+	{
+		tickRedNeuronal();
+	}
+	return ticks;
+}
+
+bool CController::genetico(){
+	//update the stats to be used in our stat window
+	m_vecAvFitness.push_back(m_pGA->AverageFitness());
+	m_vecBestFitness.push_back(m_pGA->BestFitness());
+
+		//increment the generation counter
+	++m_iGenerations;
+
+		//reset cycles
+	m_iTicks = 0;
+
+		//run the GA to create a new population
+	m_vecThePopulation = m_pGA->Epoch(m_vecThePopulation, m_iGenerations);
+
+		//insert the new (hopefully)improved brains back into the sweepers
+    //and reset their positions etc
+	for (int i=0; i<m_NumUnidades; ++i)
+	{
+		m_vecUnidades[i]->PutWeights(m_vecThePopulation[i].vecWeights);
+		modificarUnidad(m_vecUnidades[i]);
+
+	}
+}
 
 //-------------------------------------Update-----------------------------
 //
@@ -61,125 +171,28 @@ CController::~CController()
 //
 //	The comments should explain what is going on adequately.
 //-------------------------------------------------------------------------
-bool CController::Update()
+/*bool CController::Update()
 {
 	//run the sweepers through CParams::iNumTicks amount of cycles. During
   //this loop each sweepers NN is constantly updated with the appropriate
   //information from its surroundings. The output from the NN is obtained
   //and the sweeper is moved. If it encounters a mine its fitness is
   //updated appropriately,
-	if (m_iTicks++ < CParams::iNumTicks)
-	{
-		outfile.open("GeneticMovimientos.txt", ios::app);
-						if (outfile.is_open())
-						{
-							outfile<<"##########################################################CController::Update:76"<<" mUnidades "<<m_NumUnidades<<" Ticks "<<m_iTicks<<"#####################################################"<<endl;
-							
-						}
-
-			outfile.close();
-			
-		for (int i=0; i<m_NumUnidades; ++i)
-		{
-			Pintar();
-			//update the NN and position
-			outfile.open("GeneticMovimientos.txt", ios::app);
-						if (outfile.is_open())
-						{
-							outfile << "La unidad ANTES : "<<i<<" tiene de Fitness :"<<m_vecUnidades[i]->Fitness()<<" y esta en la posición: ("<<m_vecUnidades[i]->Position().x <<","<<m_vecUnidades[i]->Position().y<<")"<<endl;
-							
-						}
-
-			outfile.close();
-			if (!m_vecUnidades[i]->Update(Matriz))
-			{
-				//error in processing the neural net
-				cout<<"Wrong amount of NN inputs!"<<endl;
-				return false;
-			}
-			
-			outfile.open("GeneticMovimientos.txt", ios::app);
-						if (outfile.is_open())
-						{
-							outfile << "La unidad : "<<i<<" tiene de Fitness :"<<m_vecUnidades[i]->Fitness()<<" y esta en la posición: ("<<m_vecUnidades[i]->Position().x <<","<<m_vecUnidades[i]->Position().y<<")"<<endl;
-							
-						}
-
-			outfile.close();
-			
-			if(m_vecUnidades[i]->getAtaque()==1){
-				SVector2D atacando=m_vecUnidades[i]->getAtaqueMovimiento();
-
-				if(Matriz[atacando.y][atacando.x]!=NULL && Matriz[atacando.y][atacando.x]->getTipo()==3){
-					m_vecUnidades[i]->IncrementFitness();
-				
-      				outfile.open("GeneticMovimientos.txt", ios::app);
-						if (outfile.is_open())
-						{
-							
-							outfile << "Y esta atacando a: ("<<atacando.x<<","<<atacando.y<<")"<<endl;
-						}
-
-						outfile.close();
-				
-				}
-		        
-			
-			}
-			else{
-
-				if(m_vecUnidades[i]->getMover()==1){
-					video::IVideoDriver* driver = device->getVideoDriver();
-					Matriz[m_vecUnidades[i]->Position().y][m_vecUnidades[i]->Position().x]= new Suelo(0,m_vecUnidades[i]->Position().x,m_vecUnidades[i]->Position().y);
-					((Suelo*) Matriz[m_vecUnidades[i]->Position().y][m_vecUnidades[i]->Position().x])->setIsometric(false);
-					Matriz[m_vecUnidades[i]->Position().y][m_vecUnidades[i]->Position().x]->aplicarTextura(driver);
-					m_vecUnidades[i]->setPosition(m_vecUnidades[i]->getMovimiento());
-					Matriz[m_vecUnidades[i]->Position().y][m_vecUnidades[i]->Position().x]=m_vecUnidades[i];
-				}
-			}
-
-
-      				
-
-			//update the chromos fitness score
-			m_vecThePopulation[i].dFitness = m_vecUnidades[i]->Fitness();
-
-		}
-	}
+	
 
 	//Another generation has been completed.
-  
+
 	//Time to run the GA and update the sweepers with their new NNs
 	else
 	{
-		//update the stats to be used in our stat window
-		m_vecAvFitness.push_back(m_pGA->AverageFitness());
-		m_vecBestFitness.push_back(m_pGA->BestFitness());
-
-		//increment the generation counter
-		++m_iGenerations;
-
-		//reset cycles
-		m_iTicks = 0;
-
-		//run the GA to create a new population
-		m_vecThePopulation = m_pGA->Epoch(m_vecThePopulation, m_iGenerations);
-			
-		//insert the new (hopefully)improved brains back into the sweepers
-    //and reset their positions etc
-    for (int i=0; i<m_NumUnidades; ++i)
-		{
-			m_vecUnidades[i]->PutWeights(m_vecThePopulation[i].vecWeights);
-			modificarUnidad(m_vecUnidades[i]);
-			
-		}
+		
 		Pintar();
 	}
 
 	
 
 	return true;
-}
+}*/
 //Devuelve la unidad que hay en esa posición 
 CUnidadesAprendizaje* CController::getUnidadPosicion(SVector2D pos){
 	
@@ -191,25 +204,25 @@ void CController::Pintar()
 {
 	video::IVideoDriver* driver = device->getVideoDriver();
 	if (device->run())
-    {        
-        if(driver)
-        {
+	{        
+		if(driver)
+		{
 			position2di GridPosition, DrawPosition;
 			
-						
-		    for(int i = 0; i < MAPSIZE; i++)
-		    {
+
+			for(int i = 0; i < MAPSIZE; i++)
+			{
 				for(int j = 0; j < MAPSIZE; j++)
 				{
 					// Obtenermos coordenadas actuales cuadricula
-		            DrawPosition = position2di(j*TILE_H, i * TILE_W);
+					DrawPosition = position2di(j*TILE_H, i * TILE_W);
 					//DrawPosition = getIsoFromTile(i - CameraScroll.X, j - CameraScroll.Y);
 					// position2di((i*TILE_WIDTH) - CameraScroll.X, (j*TILE_HEIGHT) - CameraScroll.Y);
 					// Validar coordenada
-						IDibujable *Tile = Matriz[i][j];
+					IDibujable *Tile = Matriz[i][j];
 						//Pinta
-						if(Tile->getTextura())
-							Tile->Pintar(driver, DrawPosition.X, DrawPosition.Y);
+					if(Tile->getTextura())
+						Tile->Pintar(driver, DrawPosition.X, DrawPosition.Y);
 				}
 			}
 
@@ -219,18 +232,18 @@ void CController::Pintar()
 			}
 
 			device->getGUIEnvironment()->drawAll();
-			    	
-        }
-    }
-    else
-    {
-    	gameEngine::stado.finish();
-    }
+
+		}
+	}
+	else
+	{
+		gameEngine::stado.finish();
+	}
 }
 
 
 void CController::generarMapa(){
-int j=0;
+	int j=0;
 	for (int i=0;i<MAPSIZE;i++){
 		for(int j=0;j<MAPSIZE;j++){
 			//0 transitable 1 no transitable
@@ -281,8 +294,8 @@ void CController::modificarUnidad(CUnidadesAprendizaje* unidad){
 	y=0;
 
 	do{
-	x= RandInt(0,5);
-	y=RandInt(0,5);
+		x= RandInt(0,5);
+		y=RandInt(0,5);
 
 		if(Matriz[y][x]->getTipo()==0){
 			unidad->setPosition(SVector2D(x,y));
